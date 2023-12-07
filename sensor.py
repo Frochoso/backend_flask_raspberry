2bf5674635f386c0ebd62a4911a52e35fbf9af25
Frochoso <touchdownboy585@gmail.com>
2023-12-07 13:47:10 +0000

    cambios

--- sensor.py
+++ sensor.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 import dht11
 
 # initialize GPIO
@@ -10,9 +11,11 @@ GPIO.cleanup()
 instance = dht11.DHT11(17)
 result = instance.read()
 
-
-if result.is_valid():
-    print("Temperature: %-3.1f C" % result.temperature)
-    print("Humidity: %-3.1f %%" % result.humidity)
-else:
-    print("Error: %d" % result.error_code)
+while True:
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+        time.sleep(2)
+        result=instance.read()
+    else:
+        print("Error: %d" % result.error_code)
