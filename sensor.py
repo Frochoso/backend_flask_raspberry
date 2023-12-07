0cd9daab688ba03eac204a6e914ea3124f101e28
Frochoso <touchdownboy585@gmail.com>
2023-12-07 14:43:58 +0000

    LED se enciende con cada lectura nueva

--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 #activar entorno de desarrollo en bash: source env/bin/activate
 #desactivar entorno de desarrollo en bash: deactivate
 import RPi.GPIO as GPIO
+from gpiozero import LED
 import time
 import dht11
 
@@ -10,6 +11,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 # read data using pin 17
+led = LED(27)
 instance = dht11.DHT11(17)
 result = instance.read()
 
@@ -19,6 +21,12 @@ while True:
     if result.is_valid():
         print("Temperature: %-3.1f C" % result.temperature)
         print("Humidity: %-3.1f %%" % result.humidity)
+        print("LED turned ON")
+        led.on()
+        time.sleep(1)
+        led.off()
+        '''
     else:
         print("Error: %d" % result.error_code)
+        '''
     time.sleep(2)
