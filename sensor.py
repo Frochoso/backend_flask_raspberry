65d0f5a5f50bdfa04d48eecd3384f26c8fb72a80
Frochoso <touchdownboy585@gmail.com>
2023-12-07 14:04:23 +0000

    Mensajitos

--- sensor.py
+++ sensor.py
@@ -1,3 +1,5 @@
+#activar entorno de desarrollo en bash: source env/bin/activate
+#desactivar entorno de desarrollo en bash: deactivate
 import RPi.GPIO as GPIO
 import time
 import dht11
@@ -11,12 +13,12 @@ GPIO.cleanup()
 instance = dht11.DHT11(17)
 result = instance.read()
 
+time.sleep(1)  # Agrega un pequeño retraso antes de la primera lectura
 while True:
+    result = instance.read()
     if result.is_valid():
         print("Temperature: %-3.1f C" % result.temperature)
         print("Humidity: %-3.1f %%" % result.humidity)
-        time.sleep(2)
-        result=instance.read()
     else:
-        
         print("Error: %d" % result.error_code)
+    time.sleep(2)
